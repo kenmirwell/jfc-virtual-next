5181a7a77608d0bfa2cd5a3c2bb14722d9ac7295
Randjae Galvan <galvanrandjae@gmail.com>
2023-11-02 03:06:26 +0800

    Add loader

--- app/Components/FirstWorld.js
+++ app/Components/FirstWorld.js
@@ -184,7 +184,11 @@ const FirstWorld = () => {
         }
     }, [model3d]);
 
-    return null;
+    return (
+        <div className={`${ model3d ? "!opacity-0 !pointer-events-none" : "" } opacity-100 fixed top-0 left-0 right-0 bottom-0 bg-white flex items-center justify-center transition-all duration-[1s] ease-in-out z-[10]`}>
+            <div className="loader"></div>
+        </div>
+    );
 }
 
 export default FirstWorld
