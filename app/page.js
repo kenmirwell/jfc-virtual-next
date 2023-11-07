c96bd7bcd313296569d3f757be83dab69b545ec7
Randjae Galvan <galvanrandjae@gmail.com>
2023-11-08 02:18:29 +0800

    Apply 4 worlds

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
-import World1 from './Components/Worlds/World1/World1'
-import './style/app.scss'
+import World from './Components/Worlds/World1/World1'
 
 export default function Home() {
     return (
-        <World1 />
+        <World />
     )
 }
