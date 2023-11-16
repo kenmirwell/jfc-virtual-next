a7d64c27f2aef5f7bd208a7ef2d882a86339ba6b
Ken <ken@jsrwebsolution.com>
2023-11-17 01:13:07 +0800

    update

--- app/Components/Worlds/World/Background/Background.js
+++ app/Components/Worlds/World/Background/Background.js
@@ -1,6 +1,6 @@
 const Background = ({ background }) => {
     return (
-        <div className={`pointer-events-none z-[-1] overflow-hidden fixed top-0 left-0 right-0 bottom-0 bg-white flex items-center justify-center transition-all duration-[1s] ease-in-out`}>
+        <div className={`world-background pointer-events-none z-[-1] overflow-hidden fixed top-0 left-0 right-0 bottom-0 bg-white flex items-center justify-center transition-all duration-[1s] ease-in-out`}>
             <video autoPlay muted loop>
                 <source src={ background } type="video/mp4" />
             </video>
--- app/Components/Worlds/World/Flats/Flats.js
+++ app/Components/Worlds/World/Flats/Flats.js
@@ -5,7 +5,7 @@ const Flats = ({
     color
 }) => {
     return (
-        <div className={`pointer-events-none overflow-hidden z-[0] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center transition-all duration-[1s] ease-in-out`}>
+        <div className={`flats pointer-events-none overflow-hidden z-[0] fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center transition-all duration-[1s] ease-in-out`}>
             <div className="absolute top-0 left-[20px]">
                 <div id="clogo" className="translate-y-[-100px]">
                     <img src={ flats.logo } />
@@ -26,7 +26,7 @@ const Flats = ({
             <div id="cicons" className="absolute bottom-[-70px] left-[-65px] translate-y-[200px]">
                 <img src={ flats.icons } />
             </div>
-            <div id="cchapter" className="absolute top-[90px] right-[110px] translate-x-[400px]">
+            <div id="cchapter" className="chapter-icon flex justify-right absolute top-[90px] right-[110px] translate-x-[400px]">
                 <img src={ flats.chapter } width="200" />
             </div>
         </div>
