e62b2b4e4d89af3c0950f4b95d5b4e14ddce9eed
Ken <ken@jsrwebsolution.com>
2023-11-17 17:50:07 +0800

    update

--- app/Components/Worlds/World/Prompt/Prompt.js
+++ app/Components/Worlds/World/Prompt/Prompt.js
@@ -36,7 +36,7 @@ const Prompt = ({ showJoy, flow, currentFlow, onClickInteractables }) => {
                     <source src="/assets/joy/idle.webm" type="video/webm" />
                 </video>
                 <video muted autoPlay loop className={`joy-wave absolute top-0 ml-[-63px] w-[900px] max-w-none opacity-0 ${ showJoy && currentFlow.get.action === "START" ? "opacity-100" : "" }`}>
-                    <source src="/assets/joy/wave.webm" type="video/webm" />
+                    <source src="/assets/joy/wave-2.webm" type="video/webm" />
                 </video>
                 <video muted autoPlay loop className={`joy-point absolute top-0 ml-[-63px] w-[900px] max-w-none opacity-0 ${ showJoy && currentFlow.get.action === "GOTO" ? "opacity-100" : "" }`}>
                     <source src="/assets/joy/point-go.webm" type="video/webm" />
