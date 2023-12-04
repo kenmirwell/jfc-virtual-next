15189988b2be27f949992703b283dcc07389bbe4
Joshua Salazar <joshuasalazar.design@gmail.com>
2023-12-04 14:19:32 +0800

    Add poster

--- app/Components/Worlds/World/Background/Background.js
+++ app/Components/Worlds/World/Background/Background.js
@@ -1,7 +1,7 @@
 const Background = ({ background }) => {
     return (
         <div className="absolute z-[-1] overflow-hidden pointer-events-none w-full aspect-video transition-all duration-[0.5s] ease-out ">
-            <video autoPlay muted loop>
+            <video autoPlay muted loop poster="/posters/bgposter.jpg">
                 <source src={background} type="video/mp4" className="w-full" />
             </video>
         </div>
