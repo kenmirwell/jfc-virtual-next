0ca22865d4a5e623eb57cff1e800b84db2f7788e
Joshua Salazar <joshuasalazar.design@gmail.com>
2023-12-04 15:40:58 +0800

    Remove posters

--- app/Components/Worlds/World/Background/Background.js
+++ app/Components/Worlds/World/Background/Background.js
@@ -1,7 +1,7 @@
 const Background = ({ background }) => {
     return (
         <div className="absolute z-[-1] overflow-hidden pointer-events-none w-full aspect-video transition-all duration-[0.5s] ease-out ">
-            <video autoPlay muted loop poster="/posters/bgposter.jpg">
+            <video autoPlay muted loop>
                 <source src={background} type="video/mp4" className="w-full" />
             </video>
         </div>
