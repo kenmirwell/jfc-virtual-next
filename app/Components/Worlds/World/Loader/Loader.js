b5798032e662a2b57a0bcd0f2a6abdd73e53a92a
Jason Lapina <jasonlapina.blink@gmail.com>
2023-12-05 13:35:47 +0800

    add poster to loading videeo

--- app/Components/Worlds/World/Loader/Loader.js
+++ app/Components/Worlds/World/Loader/Loader.js
@@ -11,26 +11,33 @@ const Loader = ({ model3d, value }) => {
         className='z-10 absolute overflow-hidden pointer-events-none w-full aspect-video transition-all duration-[0.5s] ease-out 
       flex items-center justify-center'
       >
-        <VStack className='w-auto h-[60%]'>
-          <img
+        <video
+          autoPlay
+          muted
+          loop
+          poster='https://frdmqigbelepsdgiecdr.supabase.co/storage/v1/object/public/Worlds/Jollibee_Loading_Icon%20(1).png'
+          className='h-full max-w-[40%]'
+        >
+          <source src='https://frdmqigbelepsdgiecdr.supabase.co/storage/v1/object/public/Worlds/Jollibee_Loading_Icon%20(1).webm' />
+        </video>
+        {/* <img
             src='https://frdmqigbelepsdgiecdr.supabase.co/storage/v1/object/public/Worlds/Jollibee_Loading_Icon%20(1).png'
             alt='loading icon'
             className='w-auto h-full'
-          />
+          /> */}
 
-          <Progress
-            pos='absolute'
-            bottom='8%'
-            left='50%'
-            transform='translateX(-50%)'
-            h='8px'
-            borderRadius='20px'
-            w='60%'
-            value={value}
-            hasStripe
-            colorScheme='red'
-          />
-        </VStack>
+        <Progress
+          pos='absolute'
+          bottom='8%'
+          left='50%'
+          transform='translateX(-50%)'
+          h='8px'
+          borderRadius='20px'
+          w='60%'
+          value={value}
+          hasStripe
+          colorScheme='red'
+        />
       </div>
     </ChakraProvider>
   );
