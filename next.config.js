5185da1d8df76766da839ad4ce6e33cd07797be6
Jason Lapina <jasonlapina.blink@gmail.com>
2023-12-07 16:35:21 +0800

    add security headers in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
 const nextConfig = {
   output: "export",
+  headers: securityHeaders,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
