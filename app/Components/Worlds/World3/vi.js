5a14d844e17cd67543d62581664cc037d8663751
Jason Lapina <jasonlapina.blink@gmail.com>
2023-12-21 10:59:53 +0800

    w3 vi text fix

--- app/Components/Worlds/World3/vi.js
+++ app/Components/Worlds/World3/vi.js
@@ -4,7 +4,7 @@ export const vi = {
       "Giấc mơ trờ thành hiện thực: Công ty kinh doanh mảng nhà hàng lớn nhất Châu Á",
     description: `
             <p>Tập đoàn Jollibee đạt được tầm nhìn trở thành Công ty kinh doanh mảng nhà hàng lớn nhất châu Á.</p><br/>
-            <p>Tập đoàn Jollibee khánh thành JWS Logistics: một trong những cơ sở hậu cần nhà hàng QSR tự động lớn nhất Philippines.</p>
+            <p>Tập đoàn Jollibee khánh thành <strong>JWS Logistics</strong>: một trong những cơ sở hậu cần nhà hàng QSR tự động lớn nhất Philippines.</p>
         `,
   },
   2014: {
@@ -27,22 +27,22 @@ export const vi = {
   2018: {
     title: "Ẩm Thực Châu Á Cao Cấp: Tim Ho Wan & Panda Express",
     description: `
-            <p>Tim Ho Wan, ngôi sao Michelin, thông qua khoản đầu tư vào Titan Dining Fund, gia nhập Tập đoàn Jollibee. Có nguồn gốc từ Hong Kong, Tim Ho Wan nổi tiếng với món bánh bao thịt heo và các món dimsum đa dạng.</p><br/>
-            <p>Panda Express Philippines gia nhập Tập đoàn Jollibee thông qua liên doanh, mở cửa hàng đầu tiên tại SM Megamall vào năm 2019. Đây là mô hình nhà hàng ăn châu Á lớn nhất ở Mỹ và hiện có hơn 15 cửa hàng ở Philippines và đang phát triển.</p><br/>
+            <p><strong>Tim Ho Wan</strong>, ngôi sao Michelin, thông qua khoản đầu tư vào Titan Dining Fund, gia nhập Tập đoàn Jollibee. Có nguồn gốc từ Hong Kong, Tim Ho Wan nổi tiếng với món bánh bao thịt heo và các món dimsum đa dạng.</p><br/>
+            <p><strong>Panda Express</strong> Philippines gia nhập Tập đoàn Jollibee thông qua liên doanh, mở cửa hàng đầu tiên tại SM Megamall vào năm 2019. Đây là mô hình nhà hàng ăn châu Á lớn nhất ở Mỹ và hiện có hơn 15 cửa hàng ở Philippines và đang phát triển.</p><br/>
             <p>Tập đoàn Jollibee mở Văn phòng Công ty Davao.</p><br/>
-            <p>Tập đoàn Jollibee mở cửa hàng đầu tiên ở châu Âu với Jollibee Milan. Nằm gần Duomo, buổi khai trương đã được chào đón bởi hàng dài khách hàng - hiện tượng luôn phổ biến mỗi khi Jollibee khai trương mới.</p>
+            <p>Tập đoàn Jollibee mở cửa hàng đầu tiên ở châu Âu với <strong>Jollibee Milan</strong>. Nằm gần Duomo, buổi khai trương đã được chào đón bởi hàng dài khách hàng - hiện tượng luôn phổ biến mỗi khi Jollibee khai trương mới.</p>
         `,
   },
   2019: {
     title: "CBTL: Thương vụ mua lại đa quốc gia lớn nhất của JFC",
     description: `
-            <p>The Coffee Bean and Tea Leaf gia nhập Tập đoàn Jollibee với tư cách là thương vụ mua lại lớn nhất và đa quốc gia nhất từ trước đến nay. Thương hiệu Coffee Bean and Tea Leaf có hơn 1000 cửa hàng trên toàn thế giới.</p><br/>
+            <p><strong>The Coffee Bean and Tea Leaf</strong> gia nhập Tập đoàn Jollibee với tư cách là thương vụ mua lại lớn nhất và đa quốc gia nhất từ trước đến nay. Thương hiệu Coffee Bean and Tea Leaf có hơn 1000 cửa hàng trên toàn thế giới.</p><br/>
             <p>Jollibee mở văn phòng đại diện hiện đại tại Việt Nam, có khả năng tiếp nhận 300 cửa hàng.</p><br/>
             <p>Jollibee thành lập trụ sở Bắc Mỹ tại West Covina, California.</p>
         `,
   },
   "2020A": {
     title: "Tortazo: Hương vị sôi động của Mexico",
-    description: `<p>Tortazo gia nhập Tập đoàn Jollibee với sự hợp tác của đầu bếp Rick Bayless, người từng bảy lần đoạt giải James Beard. Tortazo cung cấp thực đơn chọn lọc gồm bánh tortas, chilaquiles, cơm trộn, salad và nhiều món khác với sứ mệnh mang đến sự khám phá thú vị về hương vị Mexico đích thực đến Mỹ.</p>`,
+    description: `<p><strong>Tortazo</strong> gia nhập Tập đoàn Jollibee với sự hợp tác của đầu bếp Rick Bayless, người từng bảy lần đoạt giải James Beard. Tortazo cung cấp thực đơn chọn lọc gồm bánh tortas, chilaquiles, cơm trộn, salad và nhiều món khác với sứ mệnh mang đến sự khám phá thú vị về hương vị Mexico đích thực đến Mỹ.</p>`,
   },
 };
