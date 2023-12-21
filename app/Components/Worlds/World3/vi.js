ff87b1ec4dcbd875447dbd71d917908f56ff4fed
Jason Lapina <jasonlapina.blink@gmail.com>
2023-12-21 10:04:30 +0800

    vi world 3 text spaces

--- app/Components/Worlds/World3/vi.js
+++ app/Components/Worlds/World3/vi.js
@@ -1,46 +1,48 @@
 export const vi = {
-    "2013": {
-        title: "Giấc mơ trờ thành hiện thực: Công ty kinh doanh mảng nhà hàng lớn nhất Châu Á",
-        description: `
-            <p>Tập đoàn Jollibee đạt được tầm nhìn trở thành Công ty kinh doanh mảng nhà hàng lớn nhất châu Á.</p>
+  2013: {
+    title:
+      "Giấc mơ trờ thành hiện thực: Công ty kinh doanh mảng nhà hàng lớn nhất Châu Á",
+    description: `
+            <p>Tập đoàn Jollibee đạt được tầm nhìn trở thành Công ty kinh doanh mảng nhà hàng lớn nhất châu Á.</p><br/>
             <p>Tập đoàn Jollibee khánh thành JWS Logistics: một trong những cơ sở hậu cần nhà hàng QSR tự động lớn nhất Philippines.</p>
-        `
-    },
-    "2014": {
-        title: "Phát triển lớn mạnh hơn: 5 Công ty kinh doanh mảng nhà hàng hàng đầu thế giới",
-        description: `
-            <p>Tập đoàn Jollibee mở Văn phòng Công ty Cebu để phục vụ nhu cầu ngày càng tăng của khu vực Visayas.</p>
-            <p>Với tinh thần mơ ước Tập đoàn Jollibee đặt mục tiêu cao hơn nữa, Trở thành một trong 5 Công ty kinh doanh mảng nhà hàng hàng đầu thế giới</p>
+        `,
+  },
+  2014: {
+    title:
+      "Phát triển lớn mạnh hơn: 5 Công ty kinh doanh mảng nhà hàng hàng đầu thế giới",
+    description: `
+            <p>Tập đoàn Jollibee mở Văn phòng Công ty Cebu để phục vụ nhu cầu ngày càng tăng của khu vực Visayas.</p><br/>
+            <p>Với tinh thần mơ ước Tập đoàn Jollibee đặt mục tiêu cao hơn nữa, Trở thành một trong 5 Công ty kinh doanh mảng nhà hàng hàng đầu thế giới</p><br/>
             <p>Tập đoàn Jollibee và Đại học Ateneo de Manila hợp tác thành lập Đại học JFC. Kể từ khi thành lập, Đại học JFC đã cung cấp trải nghiệm học tập toàn diện cho hơn 560 thành viên đội ngũ vận hành.</p>
-        `
-    },
-    "2015": {
-        title: "Smashburger: Tiên phong cho dòng bánh burger ngon hơn",
-        description: `<p>Smashburger gia nhập Tập đoàn Jollibee. Được thành lập tại Denver, Colorado, thương hiệu này là một trong những thương hiệu tiên phong trong danh mục burger ngon hơn và ngày nay có hơn 240 địa điểm trên toàn thế giới phục vụ Smashburger.</p>`
-    },
-    "2017": {
-        title: "C-JOY: Nhà máy chế biến thịt gia cầm Philippine",
-        description: `<p>Tập đoàn Jollibee và Cargill khánh thành Nhà máy chế biến thịt gia cầm Cargill Joy, hay C-JOY, ở Santo Tomas, Batangas. Vào thời điểm khánh thành, C-JOY là nhà máy chế biến thịt gia cầm lớn nhất Philippines.</p>`
-    },
-    "2018": {
-        title: "Ẩm Thực Châu Á Cao Cấp: Tim Ho Wan & Panda Express",
-        description: `
-            <p>Tim Ho Wan, ngôi sao Michelin, thông qua khoản đầu tư vào Titan Dining Fund, gia nhập Tập đoàn Jollibee. Có nguồn gốc từ Hong Kong, Tim Ho Wan nổi tiếng với món bánh bao thịt heo và các món dimsum đa dạng.</p>
-            <p>Panda Express Philippines gia nhập Tập đoàn Jollibee thông qua liên doanh, mở cửa hàng đầu tiên tại SM Megamall vào năm 2019. Đây là mô hình nhà hàng ăn châu Á lớn nhất ở Mỹ và hiện có hơn 15 cửa hàng ở Philippines và đang phát triển.</p>
-            <p>Tập đoàn Jollibee mở Văn phòng Công ty Davao.</p>
+        `,
+  },
+  2015: {
+    title: "Smashburger: Tiên phong cho dòng bánh burger ngon hơn",
+    description: `<p>Smashburger gia nhập Tập đoàn Jollibee. Được thành lập tại Denver, Colorado, thương hiệu này là một trong những thương hiệu tiên phong trong danh mục burger ngon hơn và ngày nay có hơn 240 địa điểm trên toàn thế giới phục vụ Smashburger.</p>`,
+  },
+  2017: {
+    title: "C-JOY: Nhà máy chế biến thịt gia cầm Philippine",
+    description: `<p>Tập đoàn Jollibee và Cargill khánh thành Nhà máy chế biến thịt gia cầm Cargill Joy, hay C-JOY, ở Santo Tomas, Batangas. Vào thời điểm khánh thành, C-JOY là nhà máy chế biến thịt gia cầm lớn nhất Philippines.</p>`,
+  },
+  2018: {
+    title: "Ẩm Thực Châu Á Cao Cấp: Tim Ho Wan & Panda Express",
+    description: `
+            <p>Tim Ho Wan, ngôi sao Michelin, thông qua khoản đầu tư vào Titan Dining Fund, gia nhập Tập đoàn Jollibee. Có nguồn gốc từ Hong Kong, Tim Ho Wan nổi tiếng với món bánh bao thịt heo và các món dimsum đa dạng.</p><br/>
+            <p>Panda Express Philippines gia nhập Tập đoàn Jollibee thông qua liên doanh, mở cửa hàng đầu tiên tại SM Megamall vào năm 2019. Đây là mô hình nhà hàng ăn châu Á lớn nhất ở Mỹ và hiện có hơn 15 cửa hàng ở Philippines và đang phát triển.</p><br/>
+            <p>Tập đoàn Jollibee mở Văn phòng Công ty Davao.</p><br/>
             <p>Tập đoàn Jollibee mở cửa hàng đầu tiên ở châu Âu với Jollibee Milan. Nằm gần Duomo, buổi khai trương đã được chào đón bởi hàng dài khách hàng - hiện tượng luôn phổ biến mỗi khi Jollibee khai trương mới.</p>
-        `
-    },
-    "2019": {
-        title: "CBTL: Thương vụ mua lại đa quốc gia lớn nhất của JFC",
-        description: `
-            <p>The Coffee Bean and Tea Leaf gia nhập Tập đoàn Jollibee với tư cách là thương vụ mua lại lớn nhất và đa quốc gia nhất từ trước đến nay. Thương hiệu Coffee Bean and Tea Leaf có hơn 1000 cửa hàng trên toàn thế giới.</p>
-            <p>Jollibee mở văn phòng đại diện hiện đại tại Việt Nam, có khả năng tiếp nhận 300 cửa hàng.</p>
+        `,
+  },
+  2019: {
+    title: "CBTL: Thương vụ mua lại đa quốc gia lớn nhất của JFC",
+    description: `
+            <p>The Coffee Bean and Tea Leaf gia nhập Tập đoàn Jollibee với tư cách là thương vụ mua lại lớn nhất và đa quốc gia nhất từ trước đến nay. Thương hiệu Coffee Bean and Tea Leaf có hơn 1000 cửa hàng trên toàn thế giới.</p><br/>
+            <p>Jollibee mở văn phòng đại diện hiện đại tại Việt Nam, có khả năng tiếp nhận 300 cửa hàng.</p><br/>
             <p>Jollibee thành lập trụ sở Bắc Mỹ tại West Covina, California.</p>
-        `
-    },
-    "2020A": {
-        title: "Tortazo: Hương vị sôi động của Mexico",
-        description: `<p>Tortazo gia nhập Tập đoàn Jollibee với sự hợp tác của đầu bếp Rick Bayless, người từng bảy lần đoạt giải James Beard. Tortazo cung cấp thực đơn chọn lọc gồm bánh tortas, chilaquiles, cơm trộn, salad và nhiều món khác với sứ mệnh mang đến sự khám phá thú vị về hương vị Mexico đích thực đến Mỹ.</p>`
-    },
-}
+        `,
+  },
+  "2020A": {
+    title: "Tortazo: Hương vị sôi động của Mexico",
+    description: `<p>Tortazo gia nhập Tập đoàn Jollibee với sự hợp tác của đầu bếp Rick Bayless, người từng bảy lần đoạt giải James Beard. Tortazo cung cấp thực đơn chọn lọc gồm bánh tortas, chilaquiles, cơm trộn, salad và nhiều món khác với sứ mệnh mang đến sự khám phá thú vị về hương vị Mexico đích thực đến Mỹ.</p>`,
+  },
+};
