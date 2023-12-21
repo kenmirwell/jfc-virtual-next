c58c772fe2953ed2b54f243953e3a7a4c1f5683b
Jason Lapina <jasonlapina.blink@gmail.com>
2023-12-21 10:05:42 +0800

    2004 vi text fix

--- app/Components/Worlds/World2/vi.js
+++ app/Components/Worlds/World2/vi.js
@@ -1,8 +1,8 @@
 export const vi = {
   2004: {
-    title: "The Dream: Công ty kinh doanh mảng nhà hàng số 1 Châu Á",
+    title: "The Dream: Trở thành Công ty Nhà hàng Châu Á lớn nhất",
     description: `
-            <p>Sau khi có được chỗ đứng vững chắc tại Philippines, Tập đoàn Jollibee đặt mục tiêu trở thành Công ty kinh doanh mảng nhà hàng số 1 Châu Á.</p><br/>
+            <p>Sau khi có được chỗ đứng vững chắc tại Philippines, Tập đoàn Jollibee đặt mục tiêu trở thành công ty nhà hàng châu Á lớn nhất thế giới.</p><br/>
             <p>Cùng với tầm nhìn của mình, Tập đoàn Jollibee mua lại thương hiệu quốc tế đầu tiên: <strong>Yonghe King.</strong> Ngày nay, chuỗi nhà hàng này có hơn 420 cửa hàng ở Trung Quốc và được yêu thích nhờ món đồ uống sữa đậu nành xay tươi ngon và cơm thịt lợn bằm.</p><br/>
             <p>Tập đoàn Jollibee mở Tập đoàn Thực phẩm Zenith ở Canlubang, Laguna. Vào thời điểm đó, đây là một trong những công ty lương thực lớn nhất ở Philippines và Châu Á.</p><br/>
             <p>Nhận thức được trách nhiệm cộng đồng, Tập đoàn Jollibee đã thành lập Quỹ Jollibee, ngày nay được gọi là Quỹ Tập đoàn Jollibee (JGF).</p>
